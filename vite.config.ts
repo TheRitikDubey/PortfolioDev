5d126c5ecf5337c0b9a34deade05f78ae72bf314
Ritik Dubey <www.ritikdubey@gmail.com>
2025-02-09 12:35:47 +0530

    local changes to be shown here

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This makes the app accessible from your local network
+    port: 3001, // Default Vite port (or use another if preferred)
+  },
   define: {
     'process.env': {
       VITE_API_URL: JSON.stringify(process.env.VITE_API_URL),
